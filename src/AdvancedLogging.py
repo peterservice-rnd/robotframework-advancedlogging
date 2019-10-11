60a736ee3e0ddbb6e48875cbc6c250582363eeb6
Lev.Veydenbah <Lev.Veydenbah@nexign-systems.com>
2019-10-11 10:01:25 +0400

    version 2.0.0 discontinued support for python < 3.5 added type annotations 'f-strings' usage bugfixes

--- src/AdvancedLogging.py
+++ src/AdvancedLogging.py
@@ -2,11 +2,11 @@
 
 from os.path import join as path_join, normpath
 from platform import platform
+from typing import Iterator, Union
 
 from robot.libraries.BuiltIn import BuiltIn
 from robot.libraries.OperatingSystem import OperatingSystem
 from robot.running.context import EXECUTION_CONTEXTS
-from robot.utils import is_bytes
 
 
 class AdvancedLogging(object):
@@ -14,7 +14,7 @@ class AdvancedLogging(object):
     Creating additional logs when testing.
     If during the test you want to add any additional information in the file, then this library
     provide a hierarchy of folders and files for logging.
-    Folder hierarchy is created as follows: "output_dir/test_log_folder_name/Test_Suite/Test_Suite/Test_Case/file.log".
+    Folder hierarchy is created as follows: output_dir/test_log_folder_name/Test_Suite/Test_Suite/Test_Case/file.log
     Log files and folders are not removed before the test, and overwritten of new files.
 
      == Dependency: ==
@@ -41,12 +41,12 @@ class AdvancedLogging(object):
 
     ROBOT_LIBRARY_SCOPE = 'TEST SUITE'
 
-    def __init__(self, output_dir=None, test_log_folder_name='Advanced_Logs'):
+    def __init__(self, output_dir: str = None, test_log_folder_name: str = 'Advanced_Logs') -> None:
         """ Initialisation
 
         *Args*:\n
             _output_dir_: output directory.\n
-            _test_log_folder_name_: name for log folder
+            _test_log_folder_name_: name for log folder.
         """
         self.os = OperatingSystem()
         self.bi = BuiltIn()
@@ -55,12 +55,12 @@ class AdvancedLogging(object):
         self.test_log_folder_name = test_log_folder_name
         self.win_platform = 'Windows' in platform()
 
-    def _get_suite_names(self):
+    def _get_suite_names(self) -> Iterator[str]:
         """
         Get List with the current suite name and all its parents names
 
         *Returns:*\n
-            List of the current suite name and all its parents names
+            Iterator of the current suite name and all its parents names
         """
         suite = EXECUTION_CONTEXTS.current.suite
         result = [suite.name]
@@ -70,7 +70,7 @@ class AdvancedLogging(object):
         return reversed(result)
 
     @property
-    def _suite_folder(self):
+    def _suite_folder(self) -> str:
         """ Define variables that are initialized by a call 'TestSuite'
 
         *Returns:*\n
@@ -78,7 +78,7 @@ class AdvancedLogging(object):
         """
         output = self.output_dir
 
-        if self.output_dir is None:
+        if output is None:
             output = self.bi.get_variable_value('${OUTPUT_DIR}')
 
         suite_name = path_join(*self._get_suite_names())
@@ -88,19 +88,17 @@ class AdvancedLogging(object):
             long_path_support_prefix = '\\\\?\\'
             output = long_path_support_prefix + output
 
-        suite_folder = path_join(output,
-                                 self.test_log_folder_name,
-                                 suite_name)
+        suite_folder = path_join(output, self.test_log_folder_name, suite_name)
 
         return normpath(suite_folder)
 
-    def write_advanced_testlog(self, filename, content, content_encoding='UTF-8'):
+    def write_advanced_testlog(self, filename: str, content: Union[bytes, str], content_encoding: str = 'UTF-8') -> str:
         """ Inclusion content in additional log file
 
         *Args:*\n
-        _filename_ - name of log file;
-        _content_ - content for logging;
-        _content_encoding_ - encoding of content (if it's in bytes).
+            _filename_ - name of log file;
+            _content_ - content for logging;
+            _content_encoding_ - encoding of content (if it's in bytes).
 
         *Returns:*\n
              Path to filename.
@@ -110,14 +108,14 @@ class AdvancedLogging(object):
         =>\n
         File ${OUTPUT_DIR}/Advanced_Logs/<TestSuite name>/<TestCase name>/log_for_test.log with content 'test message'
         """
-        if is_bytes(content):
+        if isinstance(content, bytes):
             content = content.decode(content_encoding)
         test_name = self.bi.get_variable_value('${TEST_NAME}', default='')
         log_file_path = path_join(self._suite_folder, test_name, filename)
         self.os.create_file(log_file_path, content)
         return normpath(log_file_path)
 
-    def create_advanced_logdir(self):
+    def create_advanced_logdir(self) -> str:
         """ Creating a folder hierarchy for TestSuite
 
         *Returns:*\n
